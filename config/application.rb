a4035e4594df3578c1e58c6180d5a5d8b1169d6f
Pedro Gaspar <pedro.gaxpar@gmail.com>
2018-12-30 23:12:32 +0000

    Take advantage of Tails' new auto-load

--- config/application.rb
+++ config/application.rb
@@ -3,8 +3,6 @@ require 'tails'
 # Allow us to require controller files without specifying the path
 $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'app', 'controllers')
 
-require 'quotes_controller'
-
 module BestQuotes
   class Application < Tails::Application
   end
