a24a99f99aaf71b150522682b47530369f5a7ec1
Pedro Gaspar <pedro.gaxpar@gmail.com>
2018-12-31 01:43:15 +0000

    Add simple view to QuotesController#a_quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,4 +1,8 @@
 class QuotesController < Tails::Controller
+  def a_quote
+    render :a_quote, noun: :winking
+  end
+
   def test
     'Hello from my controller!' + "\n<pre>\n#{env}\n</pre>"
   end
