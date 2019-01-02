80d5a2314956d047c50f39dd68278c08c9eed201
Pedro Gaspar <pedro.gaxpar@gmail.com>
2019-01-02 00:10:12 +0000

    Add update action to QuotesController

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -15,6 +15,19 @@ class QuotesController < Tails::Controller
     render :quote, obj: quote
   end
 
+  def update
+    id = params['id']
+    quote = FileModel.find(id)
+    raise "Couldn't find quote with ID: #{id.inspect}" unless quote
+
+    quote['submitter'] = params['submitter'] if params['submitter']
+    quote['quote'] = params['quote'] if params['quote']
+    quote['attribution'] = params['attribution'] if params['attribution']
+
+    quote.save
+    render :quote, obj: quote
+  end
+
   def a_quote
     render :a_quote, noun: :winking
   end
@@ -25,6 +38,8 @@ class QuotesController < Tails::Controller
   end
 
   def test
-    'Hello from my controller!' + "\n<pre>\n#{env}\n</pre>"
+    'Hello from my controller!' \
+      "\n<pre>\n#{env}\n</pre>" \
+      "\n<pre>\n#{params}\n</pre>"
   end
 end
