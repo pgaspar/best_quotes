46b6bf6ac0adaa47449a6d84b7ebf0047e8f63ed
Pedro Gaspar <pedro.gaxpar@gmail.com>
2019-01-06 16:30:48 +0000

    Test initializing and saving a record

--- sqlite_test.rb
+++ sqlite_test.rb
@@ -5,12 +5,17 @@ class MyTable < Tails::Model::SQLite; end
 
 STDERR.puts MyTable.schema.inspect
 
-# Create Row
-mt = MyTable.create('title' => 'I saw it again!')
+# Initialize and then save
+mt1 = MyTable.new
+mt1['title'] = 'Testing!'
+mt1.save!
+
+mt1 = MyTable.find mt1['id']
+puts "Title: #{mt1['title']}"
 
 puts "Count: #{MyTable.count}"
 
-top_id = mt['id'].to_i
+top_id = mt1['id'].to_i
 (1..top_id).each do |id|
   mt_id = MyTable.find(id)
   puts "Found title #{mt_id['title']}."
