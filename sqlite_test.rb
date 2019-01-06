d59a99019420d3ae5a2fc4a2912e94dd2b0b4c19
Pedro Gaspar <pedro.gaxpar@gmail.com>
2019-01-06 04:46:07 +0000

    Try out finding records

--- sqlite_test.rb
+++ sqlite_test.rb
@@ -9,3 +9,9 @@ STDERR.puts MyTable.schema.inspect
 mt = MyTable.create('title' => 'I saw it again!')
 
 puts "Count: #{MyTable.count}"
+
+top_id = mt['id'].to_i
+(1..top_id).each do |id|
+  mt_id = MyTable.find(id)
+  puts "Found title #{mt_id['title']}."
+end
